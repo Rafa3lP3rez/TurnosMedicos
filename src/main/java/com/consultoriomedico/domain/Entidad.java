7680402fa06db67134c63792976221acf726e0cf
Jeremías <95102411+RivelliDev@users.noreply.github.com>
2022-04-25 22:24:22 -0300

    Tercer commit y push jeremias

--- src/main/java/com/consultoriomedico/domain/Entidad.java
+++ src/main/java/com/consultoriomedico/domain/Entidad.java
@@ -1,7 +1,6 @@
 package com.consultoriomedico.domain;
 
 import lombok.*;
-import org.springframework.data.annotation.Id;
 
 import java.util.Date;
 
--- src/main/java/com/consultoriomedico/domain/Usuario.java
+++ src/main/java/com/consultoriomedico/domain/Usuario.java
@@ -3,7 +3,6 @@ package com.consultoriomedico.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 
 @Data
 @AllArgsConstructor
